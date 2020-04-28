94635bd9975b74a271b41cdac088c8e91e9eeb17
fisherdiede <fwd@umich.edu>
2020-04-28 02:17:49 -0400

    trying textAscent()/2

--- thesis.js
+++ thesis.js
@@ -52,7 +52,7 @@ function drawTitle() {
 	fill(255)
 	textSize(titleFontSize)
 	textAlign(CENTER);
-	text(titleString, 0, -textAscent(), windowWidth + 100, titleFontSize*1.5)
+	text(titleString, 0, -textAscent()/2.0, windowWidth + 100, titleFontSize*1.5)
 }
 
 function drawDescription() {
