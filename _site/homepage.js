f3cb1be26feee0ca388ace031a18a44f35109193
fisherdiede <fwd@umich.edu>
2020-04-28 15:42:17 -0400

    title resizing properly and set up real local testing flow

---
+++ _site/homepage.js
@@ -0,0 +1,79 @@
+var canvas;
+function preload() {
+	
+}
+
+function setup() {	
+	console.log("homepage setup")
+	canvas = createCanvas(windowWidth, windowHeight);
+	initUI()
+}
+
+function draw() {
+	background(0);
+	fill(Math.floor((mouseY/windowHeight)*255.0));
+	textFont("Courier New");
+	textSize(24);
+	textAlign(CENTER, CENTER);
+	text("welcome", 0, (windowHeight/2) - 12, windowWidth, 24)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function initUI() {
+	
+}
+
+// window.onresize = function() {
+// 	var w = windowWidth;
+// 	var h = windowHeight;  
+// 	canvas.size(w,h);
+// 	width = w;
+// 	height = h;
+// };
+
+function mousePressed() {
+	// if (loadedCount == numTracks) {
+	// 	if (isPlaying) {
+	// 		resumeDur = millis() - startTime + resumeDur
+	//    	pauseAudio()
+	//    	background(255, 255, 255);
+	//    	isPlaying = false;
+	// 	} else {
+	// 		startTime = millis() + 1000
+	//    	playAudio()
+	//    	background(0, 0, 0);
+	//    	isPlaying = true;
+	//    }
+	// } else {
+	// 	console.log("not all audio has loaded yet")
+	// }
+	
+
+	// Check if mouse is inside the circle
+	// let d = dist(mouseX, mouseY, width/2, height/2);
+	// if (d < 100) {
+	// 	// Pick new random color values
+	// 	circleColor = colors[Math.random(colors.length)];
+	// }
+	
+  	return false
+}
+
+function mouseWheel(event) {
+	// if (mouseX > trackIndent) {
+	// 	if (mouseX < trackWidth + trackIndent) {
+	// 		var bin = int(mouseY/windowHeight * float(numFirstCol()))
+	// 		updateVolume(bin, event.delta)
+	// 	}
+	// 	else if (mouseX < trackWidth*2 + trackIndent) {
+	// 		var bin = int(mouseY/windowHeight * float(numSecondCol()))
+	// 		updateVolume(numFirstCol()+bin, event.delta)    
+	// 	}
+	// }
+	return false;
+}
+
+
